c454f6540e9aa9804378630f3ba111c082be3954
Eswaridevops <eswarinalla222@gmail.com>
2025-11-22 12:16:03 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,15 @@ public static void main(String[] args) {
 System.out.println("Hello World");
 }
 }
+
+# deveoper2 push for local#
+
+  
+  public class AddTwoIntegers {
+public static void main(String[] args) {
+int first = 10;
+int second = 20;
+int sum = first + second;
+System.out.println("The sum is: " + sum);
+}
+}
